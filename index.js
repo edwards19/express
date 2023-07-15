964db6dda23a4b1a07ce6e8b75017357a862f3e4
Eduard <eduard.c@luxon-tech.com>
2023-07-15 20:31:03 +0300

    Create a route handling middleware and fix ejs structure

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ const config = {
 	dir: {
 		root: __dirname,
 		static: __dirname + 'static' + sep,
-        views: __dirname + 'views' + sep,
+		views: __dirname + 'views' + sep,
+		routes: __dirname + 'routes' + sep,
 	},
 };
 
@@ -24,6 +25,13 @@ app.disable('x-powered-by');
 
 console.dir(config, { depth: null, color: true });
 
+app.get('/author/:authorName/book/:bookName', (req, res, next) => {
+	console.log(req.params.authorName);
+	console.log(req.params.bookName);
+	res.send('Author name and book query');
+    next();
+});
+
 app.use((req, res, next) => {
 	console.log(req.url);
 	next();
@@ -34,19 +42,22 @@ app.use(compression());
 
 // home page route
 app.get('/', (req, res) => {
-	res.render('message', { title: 'Hello!' });
+	res.render('message', { title: 'Hello World!' });
 });
 
-app.get('/hello/', (req, res) => {
-	res.render( 'message', {title: 'Hello again!'});
-});
+// /hello/ route
+import { helloRouter } from './routes/hello.js';
+// app.use() defines the helloRouter middleware rather than a single app.get() route.
+app.use('/hello', helloRouter);
 
 app.use(express.static(config.dir.static));
 
+console.log('static here', config.dir.static)
+
 // 404 error
 app.use((req, res) => {
-    res.render('message', { title: 'Not found!' });
-})
+	res.render('message', { title: 'Not found!' });
+});
 
 // start server
 app.listen(config.port, () => {
